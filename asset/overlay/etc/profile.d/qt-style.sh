0240b1dd44e7b3daec7b78ce4b6fd36b226d23dc
samwhelp <samwhelp@users.noreply.github.com>
2025-01-01 14:45:30 +0800

    fix

---
+++ asset/overlay/etc/profile.d/qt-style.sh
@@ -0,0 +1,10 @@
+
+
+#export QT_QPA_PLATFORMTHEME=kvantum
+#export QT_PLATFORM_PLUGIN=kvantum
+#export QT_STYLE_OVERRIDE=kvantum
+
+
+export QT_QPA_PLATFORMTHEME=gtk2
+export QT_PLATFORM_PLUGIN=gtk2
+#export QT_STYLE_OVERRIDE=gtk2
